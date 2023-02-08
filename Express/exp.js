9f5ff6c5bbc131a48346709614d93f37ff216587
Swati Tanu <swatitani01@gmail.com>
2023-02-08 15:19:20 +0530

    updated

--- Express/exp.js
+++ Express/exp.js
@@ -25,6 +25,52 @@ app.post("/adddata", (request,response)=>{
     response.send("Data has been added");
 })
 
+app.get("/info", (request,response)=>{
+    const dataStream = fs.createReadStream("./info.json","utf-8");
+    dataStream.pipe(response);
+})
+
+app.get("/students", (request,response)=>{
+    const data= fs.readFileSync("./info.json","utf-8");
+    const parsedData = JSON.parse(data)
+    console.log(parsedData.students);
+    response.send(parsedData.students);
+})
+
+app.get("/teachers", (request,response)=>{
+    const data= fs.readFileSync("./info.json","utf-8");
+    const parsedData = JSON.parse(data)
+    console.log(parsedData.teachers);
+    response.send(parsedData.teachers);
+})
+
+app.post("/addstudents", (request,response)=>{
+    //Step 1: Get the complete data.
+    const data= fs.readFileSync("./info.json","utf-8");
+    //Step 2: Parse the data.
+    const parsedData = JSON.parse(data);
+    //Step 3: Add the data in student.
+    parsedData.students.push(request.body);
+    //Step 4: Add it to the data file.
+    fs.writeFileSync("./info.json", JSON.stringify(parsedData));
+    console.log(parsedData);
+    response.send("Data has been added, look in the terminal!");
+})
+
+app.delete("/deletestudent", (request,response)=>{
+    //Step 1: Get the complete data.
+    const data= fs.readFileSync("./info.json","utf-8");
+    //Step 2: Parse the data.
+    const parsedData = JSON.parse(data);
+    //Step 3: Delete/Filter the required data in student.
+    const newData = parsedData.students.filter((element) => {
+        return element.name !== "Munnu";
+    });
+    //Display the newData
+    console.log(newData);
+    response.send("Data has been deleted, look in the terminal!");
+})
+
 app.listen(4500, () => {
     console.log("Server is running at port 4500");
 })
