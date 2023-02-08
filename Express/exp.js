f458fcd44600902fc443f9b837be0a1350097903
Swati Tanu <swatitani01@gmail.com>
2023-02-08 14:41:59 +0530

    update

---
+++ Express/exp.js
@@ -0,0 +1,31 @@
+//Express is built over http module of node only.
+//It is not an inbuilt module of node, so we have to install it using npm.
+//Express: Fast, Unopinionated, Minimalist web framework for Node.js.
+//Several popular Node.js framework are built on Express: Eg; Nestjs, Feathers etc.
+
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+app.use(express.json()); //Middleware: Used to parse the json data.
+
+app.get("/", (request,response)=>{
+    response.setHeader("Content-Type","text/html")
+    response.end("<h1>HOME PAGE</h1>"); //response.send() will also work.
+})
+
+app.get("/data", (request,response)=>{
+    //response.setHeader("Content-Type","application/json")
+    const dataStream = fs.createReadStream("./data.json","utf-8");
+    dataStream.pipe(response);
+})
+
+app.post("/adddata", (request,response)=>{
+    console.log(request.body);
+    response.send("Data has been added");
+})
+
+app.listen(4500, () => {
+    console.log("Server is running at port 4500");
+})
+
--- Express/index.js
+++ Express/index.js
@@ -1,30 +1,45 @@
-//Express is built over http module of node only.
-//It is not an inbuilt module of node, so we have to install it using npm.
-//Express: Fast, Unopinionated, Minimalist web framework for Node.js.
-
 const express = require('express');
-const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json()); //Middleware: Used to parse the json data.
+app.use(bodyParser.urlencoded({extended:true}));
 
+//Routing
 app.get("/", (request,response)=>{
-    response.setHeader("Content-Type","text/html")
-    response.end("<h1>HOME PAGE</h1>"); //response.send() will also work.
-})
+    response.send("Landing Page");
+});
 
-app.get("/data", (request,response)=>{
-    //response.setHeader("Content-Type","application/json")
-    const dataStream = fs.createReadStream("./data.json","utf-8");
-    dataStream.pipe(response);
-})
+app.get("/about", (request,response)=>{
+    response.send("About Us");
+});
 
-app.post("/adddata", (request,response)=>{
-    console.log(request.body);
-    response.send("Data has been added");
-})
+app.get("/contact", (request,response)=>{
+    response.send("Contact Us: abc@gmail.com");
+});
 
-app.listen(4500, () => {
+app.get("/calculator", (request,response) => {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/calculator", (request,response) => {
+    let n1 = Number(request.body.v1);
+    let n2 = Number(request.body.v2);
+    let sum = n1+ n2;
+    response.send("The sum of the two number is : "+sum);
+});
+
+app.get("/bmi", (request,response) => {
+    response.sendFile(__dirname + "/bmi.html");
+});
+
+app.post("/bmi", (request,response) => {
+    let weight = Number(request.body.weight);
+    let height = Number(request.body.height);
+    let BMI = weight/(height*height);
+    response.send("The BMI is : "+BMI);
+});
+
+
+app.listen(4500, (request,response) => {
     console.log("Server is running at port 4500");
 })
-
