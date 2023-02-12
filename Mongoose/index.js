ed006a07c16b9c0f751828cede84e7a931b745a4
Swati Tanu <swatitani01@gmail.com>
2023-02-12 12:18:34 +0530

    updated

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -8,10 +8,19 @@ app.get("/",(req,res) => {
     res.send("Welcome!")
 })
 
-app.get("/users",(req,res) => {
-    res.send("All the users data will be sent.")
+//READ
+app.get("/users", async(req,res) => {
+    let query = req.query
+    try{
+    const users = await UserModel.find(query) //Helps mentioning the query
+    res.send(users)
+    }
+    catch(err){
+        res.status(500).send({"msg": "Cannot register", "error": err.message})
+    }
 })
 
+//CREATE
 app.post("/register", async(req,res) => {
     console.log(req.body)
     const user = new UserModel(req.body) 
@@ -21,6 +30,29 @@ app.post("/register", async(req,res) => {
     } catch (err) {
         console.error(err)
         res.status(500).send({"msg": "Cannot register", "error": err.message})
+    }
+})
+
+//UPDATE
+app.patch("/update/:id", async(req,res) => {
+    const ID = req.params.id
+    const payload = req.body
+    try{
+        await UserModel.findByIdAndUpdate({_id:ID},payload)
+        res.send({"msg": "User has been updated"})
+    }catch(err){
+        res.status(500).send({"msg": "Cannot modify", "error": err.message})
+    }
+})
+
+//DELETE
+app.patch("/delete/:id", async(req,res) => {
+    const ID = req.params.id
+    try{
+        await UserModel.findByIdAndDelete({_id:ID})
+        res.send({"msg": "User has been deleted"})
+    }catch(err){
+        res.status(500).send({"msg": "Cannot delete", "error": err.message})
     }
 })
 
