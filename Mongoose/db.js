698a7d0fd0309cb640a70a7a1c48ae7fe92efdcc
Swati Tanu <swatitani01@gmail.com>
2023-02-12 18:21:30 +0530

    updated

--- Mongoose/db.js
+++ Mongoose/db.js
@@ -9,21 +9,6 @@ const connection = mongoose.connect(process.env.mongoURL, {
   socketTimeoutMS: 30000
 });
 
-//Basic Structure
-const userSchema = mongoose.Schema({
-    name: {type:String, required:true},
-    age: {type:Number, required:true},
-    city: {type:String, required:true},
-    language: {type:String, required:true},
-    is_married: {type:Boolean, required:true},
-},{
-    versionKey: false
-})
-
-//Model for the data
-const UserModel = mongoose.model("user",userSchema)
-
 module.exports = {
-    connection,
-    UserModel
+    connection
 }
--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const {connection, UserModel} = require("./db")
+const {connection} = require("./db")
+const {userRouter} = require("./routes/users.routes")
 require('dotenv').config()
 
 const app = express()
@@ -9,53 +10,7 @@ app.get("/",(req,res) => {
     res.send("Welcome!")
 })
 
-//READ
-app.get("/users", async(req,res) => {
-    let query = req.query
-    try{
-    const users = await UserModel.find(query) //Helps mentioning the query
-    res.send(users)
-    }
-    catch(err){
-        res.status(500).send({"msg": "Cannot register", "error": err.message})
-    }
-})
-
-//CREATE
-app.post("/register", async(req,res) => {
-    console.log(req.body)
-    const user = new UserModel(req.body) 
-    try {
-        await user.save()
-        res.send({"msg": "User has been registered"})
-    } catch (err) {
-        console.error(err)
-        res.status(500).send({"msg": "Cannot register", "error": err.message})
-    }
-})
-
-//UPDATE
-app.patch("/update/:id", async(req,res) => {
-    const ID = req.params.id
-    const payload = req.body
-    try{
-        await UserModel.findByIdAndUpdate({_id:ID},payload)
-        res.send({"msg": "User has been updated"})
-    }catch(err){
-        res.status(500).send({"msg": "Cannot modify", "error": err.message})
-    }
-})
-
-//DELETE
-app.patch("/delete/:id", async(req,res) => {
-    const ID = req.params.id
-    try{
-        await UserModel.findByIdAndDelete({_id:ID})
-        res.send({"msg": "User has been deleted"})
-    }catch(err){
-        res.status(500).send({"msg": "Cannot delete", "error": err.message})
-    }
-})
+app.use("/users",userRouter)
 
 app.listen(process.env.port, async() => {
     try{
